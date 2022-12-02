778f74252ecb42b7fdf11f28463c748353076612
Ali Mohammad Pur <ali.mpfard@gmail.com>
2022-12-02 10:52:38 +0330

    Add a tiny script to generate skeletons for days

---
+++ day.sh
@@ -0,0 +1,24 @@
+day=$1
+shift
+
+if [ -z "$day" ]; then
+    echo "Usage: day.sh <day>"
+    exit 1
+fi
+
+cat <<EOF > day$day.jakt
+import day$day::part1 as p1
+import day$day::part2 as p2
+
+function main() {
+    p1::solution()
+    p2::solution()
+}
+EOF
+
+mkdir -p day$day
+touch day$day/part1.jakt
+touch day$day/part2.jakt
+ln -s ../utils.jakt day$day/utils.jakt
+
+git add day$day.jakt day$day
